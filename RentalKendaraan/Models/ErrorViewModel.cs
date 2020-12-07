71025314b9cc85b744b6b291cdca601d67d461f2
Aalfinuraziz <alfinuraziz885@gmail.com>
2020-12-07 20:53:15 +0700

    Add project files.

---
+++ RentalKendaraan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentalKendaraan_034.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
